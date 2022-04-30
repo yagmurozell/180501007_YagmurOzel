c42b86bac24483aa24d3338d74ae0fce1c7b641c
z004c9ma <e180501007@stud.tau.edu.tr>
2022-04-30 22:20:36 +0300

    Kunden-, Produkt- und Login-Daten wurden aus der Datenbank gezogen.

--- 180501007_YagmurOzel/src/Konnektor.java
+++ 180501007_YagmurOzel/src/Konnektor.java
@@ -1,28 +1,70 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
 
 
-public class Konnektor {
+public class Konnektor extends JFrame {
+	static Connection myConn;
+	static Statement myStat;
 
-	public static void main(String[] args) {
+	
+	public void conn() {
 		
 		try {
-			
-			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/reinigung","root", "12341234");
-			Statement myStat = (Statement) myConn.createStatement();
-			ResultSet myRs = ((java.sql.Statement) myStat).executeQuery("select * from produkt");
-			while(myRs.next()) {
-				System.out.print(myRs.getString("Produkt_id") + "-" + myRs.getString("Name") + myRs.getString("Kosten"));
-			}
-			
-			
-			
-		} catch (Exception e) {
+			myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/reinigung","root", "12341234");
+			System.out.println("Connection succesfull");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+	
 	}
 
+	
+	static ResultSet stat_produkt(){
+		ResultSet myRs = null;
+		try {
+			myStat = (Statement) myConn.createStatement ();
+			myRs = myStat.executeQuery("SELECT * FROM produkt");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return myRs;		
+	}
+
+	static ResultSet stat_kunden(){
+		ResultSet myRs = null;
+		try {
+			myStat = (Statement) myConn.createStatement ();
+			myRs = myStat.executeQuery("SELECT * FROM kunden");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return myRs;		
+	}
+
+	static ResultSet stat_login(){
+		ResultSet myRs = null;
+		try {
+			myStat = (Statement) myConn.createStatement ();
+			myRs = myStat.executeQuery("SELECT * FROM user");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return myRs;		
+	}
+
+	public static void main(String[] args) {
+
+		Konnektor x = new Konnektor();
+		x.conn();
+}
 }
